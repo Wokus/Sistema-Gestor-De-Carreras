0109db724f51bf06ff39bd7865f7ae6a0c704964
Andrés Ferreira <24ferreiraandres@gmail.com>
2025-10-15 12:01:13 -0300

    a

--- SGCarreras/SGCarreras/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SGCarreras.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
