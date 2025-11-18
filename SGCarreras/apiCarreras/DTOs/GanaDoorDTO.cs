580f1f839ab7e2a7e23d525a1ae9dbc3919a1214
Andrés Ferreira <24ferreiraandres@gmail.com>
2025-11-17 23:11:07 -0300

    Simulacion completa
    Falta la comuncacion para pasar los datos a la base de datos cuando una carrera termina, pero despues de eso ya etá

---
+++ SGCarreras/apiCarreras/DTOs/GanaDoorDTO.cs
@@ -0,0 +1,15 @@
+﻿using static apiCarreras.Controllers.CarrerasSimuladasController;
+
+namespace apiCarreras.DTOs
+{
+    public class GanaDoorDTO
+    {
+        public int registroId {  get; set; }
+        public string tiempoDeFinalizacion { get; set; }
+        public int numeroEnCarrera { get; set; }
+
+        public int numeroDelCorredor { get; set; }
+
+        public GanaDoorDTO() { }
+    }
+}
